37633e4be236ec382d7a1b69ed590d8e3ac5dc84
NIRVISHEK AWASTHI <nirvishek@smalldaytech.com>
2020-04-21 22:14:20 +0530

    added default created_date timestamp.

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Blog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     text = models.TextField()
-    created_date = models.DateTimeField(timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
